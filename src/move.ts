c57b06547513e63d9efd3f88b9ccaffaac96eb40
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2022-08-16 20:33:13 +0700

    extract own state

--- src/move.ts
+++ src/move.ts
@@ -1,5 +1,14 @@
 import { Moves, RequestBody } from "./types";
 
 export default function move({ _links, arena }: RequestBody): Moves {
-  return Moves.R;
+  const playerState = arena.state;
+  const [width, height] = arena.dims;
+
+  const maxX = width - 1;
+  const maxY = height - 1;
+
+  const myLink = _links.self.href;
+  const { x, y, direction: dir } = playerState[myLink];
+
+  return Moves.F;
 }
