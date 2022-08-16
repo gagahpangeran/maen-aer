498395a811e19c86612270aff103144b0aa47929
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2022-08-16 17:03:33 +0700

    change web.js to index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
-const express = require('express');
+import express, { Request, Response } from "express"
+import bodyParser from "body-parser";
+
 const app = express();
-const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.get('/', function (req: Request, res: Response) {
   res.send('Let the battle begin!');
 });
 
-app.post('/', function (req, res) {
+app.post('/', function (req: Request, res: Response) {
   console.log(req.body);
   const moves = ['F', 'T', 'L', 'R'];
   res.send(moves[Math.floor(Math.random() * moves.length)]);
