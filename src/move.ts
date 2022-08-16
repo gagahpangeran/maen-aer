dd4b4fe564b796d67aa55f4d7fbd9ee9f035febb
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2022-08-16 20:33:26 +0700

    add move from wall

--- src/move.ts
+++ src/move.ts
@@ -1,4 +1,20 @@
-import { Moves, RequestBody } from "./types";
+import { Direction as Dir, Moves, RequestBody } from "./types";
+
+function moveFromWall(x: number, y: number, maxX: number, maxY: number, dir: Dir) {
+  if (x === 0 && dir === Dir.W)
+    return y === 0 ? Moves.L : Moves.R;
+
+  if (x === maxX && dir === Dir.E)
+    return y === 0 ? Moves.R : Moves.L;
+
+  if (y === 0 && dir === Dir.N)
+    return x === 0 ? Moves.R : Moves.L;
+
+  if (y === maxY && dir === Dir.S)
+    return x === 0 ? Moves.L : Moves.R;
+
+  return undefined;
+}
 
 export default function move({ _links, arena }: RequestBody): Moves {
   const playerState = arena.state;
@@ -9,6 +25,11 @@ export default function move({ _links, arena }: RequestBody): Moves {
 
   const myLink = _links.self.href;
   const { x, y, direction: dir } = playerState[myLink];
+
+  const wallMove = moveFromWall(x, y, maxX, maxY, dir);
+
+  if (wallMove !== undefined)
+    return wallMove;
 
   return Moves.F;
 }
